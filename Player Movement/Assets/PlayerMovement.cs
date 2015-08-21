8f8ec65d31cb2a5b75e6452d32126f0e9bc88382
Italo Berg <italoberg@yahoo.com.br>
2015-08-21 16:54:39 -0300

    Script to move the player in 4 directions

---
+++ Player Movement/Assets/PlayerMovement.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+	public Transform heroi;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKey("left"))
+			heroi.position =new Vector3(heroi.position.x-0.1f,heroi.position.y);
+		
+		if (Input.GetKey("right"))
+			heroi.position =new Vector3(heroi.position.x+0.1f,heroi.position.y);
+
+		if (Input.GetKey("up"))
+			heroi.position =new Vector3(heroi.position.x, heroi.position.y+0.1f);
+		
+		if (Input.GetKey("down"))
+			heroi.position =new Vector3(heroi.position.x, heroi.position.y-0.1f);
+	}
+}
